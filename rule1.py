2ec473a28a7aafc9f24f87dd3f8cdcb1333a875b
lauragro <lauragro@users.noreply.github.com>
2015-03-23 16:45:31 -0700

    Update rule1.py
    fixed range for exclusivity

--- rule1.py
+++ rule1.py
@@ -8,9 +8,9 @@ def rule1(base):
     clauses = base**2
     rule += "p cnf {0} {1}\n".format(variables, clauses)
     clause = ""
-    for i in range(1, base):
-        for j in range(1, base):
-            for k in range(1, base):
+    for i in range(1, base+1):
+        for j in range(1, base+1):
+            for k in range(1, base+1):
                 variable = to_base(i, j, k, base)
                 clause += "{0} ".format(variable)
             rule += clause
