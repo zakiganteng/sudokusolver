6834061701f87354c170856ba5b7a184eb813c73
lauragro <lauragro@users.noreply.github.com>
2015-03-23 16:46:23 -0700

    Update rule3.py
    changed to a general base

--- rule3.py
+++ rule3.py
@@ -3,10 +3,10 @@ from xmath import to_base
 # Evan Wilde
 def rule3(base):
     rules = []
-    for j in range(1, 10):
-        for k in range(1, 10):
-            for i in range(1, 9):
-                for l in range(j + 1, 10):
+    for j in range(1, base+1):
+        for k in range(1, base+1):
+            for i in range(1, base):
+                for l in range(j + 1, base+1):
                     rules.append((-to_base(i, j, k, base), -to_base(l, j, k,
                         base)))
     ret_string = 'p cnf {0} {1}\n'.format(base**3, len(rules))
