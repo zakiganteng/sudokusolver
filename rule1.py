eded75aef9d9f2e323ee446ecff307cbde7bfddc
lauragro <lauragro@users.noreply.github.com>
2015-03-23 17:54:11 -0700

    Update rule1.py

--- rule1.py
+++ rule1.py
@@ -5,6 +5,7 @@ from xmath import to_index
 # Every cell contains at least one number
 def rule1(base, board):
     rules = []
+    count = 0
     for i in range(1, base+1):
         for j in range(1, base+1):
             val = board[to_index(i-1,j-1, base)]
@@ -12,10 +13,12 @@ def rule1(base, board):
             if val == '0':
                 for k in range(1, base+1):
                     ors.append(to_base(i, j, k, base))
+                    count += 1
             else:
                 ors.append(to_base(i, j, val, base))
+                count += 1
             rules.append(ors)
-    ret_string = 'p cnf {0} {1}\n'.format(base**3, len(rules))
+    ret_string = 'p cnf {0} {1}\n'.format(count, len(rules))
     for ors in rules:
         for val in ors:
             ret_string += str(val) + " "
