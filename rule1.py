fe22c1dbe24787b5a395f663441f9e14565b902d
Sebastien Guillemot <SebastienGllmt@gmail.com>
2015-03-23 17:04:44 -0700

    to_index should take a base

--- rule1.py
+++ rule1.py
@@ -7,7 +7,7 @@ def rule1(base, board):
     rules = []
     for i in range(1, base+1):
         for j in range(1, base+1):
-            val = board[to_index(i,j)]
+            val = board[to_index(i,j, base)]
             if val == '0':
                 for k in range(1, base+1):
                     rules.append(-to_base(i, j, k, base))
