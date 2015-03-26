2ec97b8f04e70cad122039b8d6939a90f12e19c9
lauragro <lauragro@users.noreply.github.com>
2015-03-26 15:18:03 -0700

    Update rule4.py
    changed to general base and created  variable to sqrt

--- rule4.py
+++ rule4.py
@@ -3,14 +3,15 @@ import math
 
 def rule4(base):
     rules = []
+    sq = int(math.sqrt(base))
     for k in range(1, base+1):
-        for a in range(int(math.sqrt(base))):
-            for b in range(int(math.sqrt(base))):
-                for u in range(1, 4):
-                    for v in range(1, 3):
-                        for w in range(v+1, 4):
-                            rules.append((-to_base(3*a + u, 3*b + v, k, base),
-                                          -to_base(3*a+u, 3*b+w, k, base)))
+        for a in range(sq):
+            for b in range(sq):
+                for u in range(1, sq+1):
+                    for v in range(1, sq):
+                        for w in range(v+1, sq+1):
+                            rules.append((-to_base(sq*a + u, sq*b + v, k, base),
+                                          -to_base(sq*a+u, sq*b+w, k, base)))
     ret_string = 'p cnf {0} {1}\n'.format(base**3, len(rules))
     for (i, j) in rules:
         ret_string += '{0} {1} 0\n'.format(i, j)
