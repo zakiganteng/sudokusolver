e32eb35c952aa5dddd7b188a9365cb34c231211a
Evan Wilde <etcwilde@uvic.ca>
2015-03-31 19:12:06 -0700

    Changing board conversion to handle other bases

--- print_functions.py
+++ print_functions.py
@@ -12,7 +12,7 @@ def print_board(b):
         print ("Error! Invalid Board")
     for y in range(0, height):
         if y % sub_width == 0 and y > 0:
-            print ("\n", end="")
+            print ()
             for x in range(0, width * 2):
                 if (x/2) % sub_width == 0 and x > 0:
                     print ("-+", end = "")
@@ -40,15 +40,33 @@ def output_board(b):
 
 
 def convert_board(b_original):
-    b_original = b_original.replace("\n", "")
+    b_original = b_original.replace("\n", "") # Do the cleaning thing
     b_original = b_original.replace("\t", "")
     b_original = b_original.replace(" ", "")
     b_original = re.sub(" \n", "", b_original)
-    return re.sub("[^0-9]", "0", b_original)
-
+    b_original = re.sub("[^0-9a-zA-Z]", "0", b_original)
+    ret_list = []
+    for char in b_original:
+        ret_list.append(int(char, 32))
+    return ret_list
 
 if __name__ == "__main__":
-    b = "1638.5.7..*8040065005007008450082039301000040700000000839050000604200590000093081"
+    b = "e4...a.....519d8\
+        .2.....b.dec..5.\
+        .8..76....b...3c\
+        .9.a...87..3..f.\
+        9...e...37fd.c.1\
+        ...f...a.e5..d..\
+        ...c...fa.....7.\
+        d7b....9..c..4.0\
+        ..84d9.c....ab.3\
+        ...9.5.768......\
+        ..d...4..a......\
+        ..f26.b0......e.\
+        .e.7c35..4.1d..6\
+        .fa.....2.6..1..\
+        .......6....0e8f\
+        8.c6a..........9"
     print_board(b)
     print()
     print_board(convert_board(b))
